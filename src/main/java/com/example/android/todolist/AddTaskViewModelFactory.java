bb58bbfae56be572bb5d440401c1a243f45492c3
Tiego Makaleng <tiego.makaleng@gmail.com>
2019-01-31 03:58:40 +0200

    The project's source code and its dependencies were added

---
+++ src/main/java/com/example/android/todolist/AddTaskViewModelFactory.java
@@ -0,0 +1,22 @@
+package com.example.android.todolist;
+
+import android.arch.lifecycle.ViewModel;
+import android.arch.lifecycle.ViewModelProvider;
+
+import com.example.android.todolist.database.AppDatabase;
+
+public class AddTaskViewModelFactory extends ViewModelProvider.NewInstanceFactory {
+
+    private final AppDatabase mDb;
+    private final int mTaskId;
+
+    public AddTaskViewModelFactory(AppDatabase database, int taskId) {
+        mDb = database;
+        mTaskId = taskId;
+    }
+
+    @Override
+    public <T extends ViewModel> T create(Class<T> modelClass) {
+        return (T) new AddTaskViewModel(mDb, mTaskId);
+    }
+}
